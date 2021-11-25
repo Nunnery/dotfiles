374e57c08f3160aef91d6f4357bc9ad5c6c51f99
Richard Harrah <topplethenunnery@gmail.com>
2021-11-24 22:11:13 -0500

    add ssh-agent on startup and fix git pushes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,9 @@ end
 
 starship init fish | source
 zoxide init fish | source
+
+if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
